730b2920a2324196ec0329ee817ec89897815bdb
prishika211 <prishikacdy10@gmail.com>
2024-10-23 15:58:21 +0530

    import dashboard router and its declaration

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ import likeRouter from "./routes/like.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import dashboradRouter from "./routes/dashboard.routes.js"
 
 
 //routes declaration
@@ -38,5 +39,6 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/dashboard", dashboradRouter);
 
 export { app } 
